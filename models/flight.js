1679c9440161b6e5aaff076f0139bef990f487a0
Sokoun Touch <sokoun01@gmail.com>
2025-11-07 10:22:37 +0700

    Add new schema Flight

---
+++ models/flight.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+const FlightSchema = new mongoose.Schema({
+  FlightNumber: { type: String, required: true, unique: true },
+  DepartmentDateTime: { type: String, required: true },
+  ArrivalDateTime: { type: String, required: true },
+  OriginAirportCode: { type: String, required: true, ref: "Airport" },
+  DestinationAirportCode: { type: String, required: true, ref: "Airport" },
+  AvailableSeats: { type: Number, required: true },
+  AirlineID: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: "Airline",
+  },
+});
+module.exports = mongoose.model("Flight", FlightSchema);
